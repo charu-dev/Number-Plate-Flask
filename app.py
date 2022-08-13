0b215ebd2dd928719999b4ca36328f92a081e887
charu-dev <charu@gupta.com>
2022-08-13 14:43:51 +0530

    working flask app

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+# import pickle
 
 # Create flask app
 flask_app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+# model = pickle.load(open("model.pkl", "rb"))
 
 @flask_app.route("/")
 def Home():
     return render_template("index.html")
 
-# @flask_app.route("/predict", methods = ["POST"])
-# def predict():
-#     float_features = [float(x) for x in request.form.values()]
-#     features = [np.array(float_features)]
-#     prediction = model.predict(features)
-#     return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    print("Cool cool")
+    prediction = "C C CHARU"
+    return render_template("index.html", prediction_text = "The predicted car number is {}".format(prediction))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
